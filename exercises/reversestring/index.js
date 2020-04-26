7ff2d55b54ef157752c6d39ba4893631fa2fff5b
Arpit Purwar <apurwa19@in.ibm.com>
2020-04-26 20:46:09 +0530

    Push work

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,38 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// function reverse(str) {
+//     let reversedString = '';
+//     for(let i = str.length-1; i>=0;i--){
+//         reversedString += str[i];
+//     }
+
+//     return reversedString;
+// }
+
+function reverse(str) {
+    return str.split('').reduce( (reversedString,character) => character + reversedString,'')
+}
+
+
+// function reverse(str) {
+//     let reversedString = '';
+//     for(let character of str ){
+//         reversedString = character + reversedString ;
+//     }
+
+//     return reversedString;
+// }
+
+// function reverse(str) {
+  
+//     return str.split('').reverse().join('');
+// }
+
+
+module.exports = reverse;
